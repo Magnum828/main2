9aa43e732f6bffc300da178ee493efe555de2c8e
Magnum828 <magnum82852@gmail.com>
2018-09-09 23:08:33 +0200

    3. Czas

---
+++ CZA.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int s;
+    cin >> s;
+    int m = s/60;
+    int h = m/60;
+    cout << h << "g" << m % 60 << "m" << s % 60 << "s";
+}
