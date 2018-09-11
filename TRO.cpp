1fc94d0e245519b9e894bf1ce69ed5ee9315c643
Magnum828 <magnum82852@gmail.com>
2018-09-11 20:55:44 +0200

    Tak się dzieje jak programuje się pod wpływem XD

--- TRO.cpp
+++ TRO.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-    int a, b, c, x, y, z;
+    int a, b, c;
     cin >> a >> b >> c;
-    x = a+b;
-    y = b+c;
-    z = a+c;
-    if(x>c && c>a && c>b)
+    
+    if(a+b>c && a+c>b && b+c>a)
         cout << "TAK";
-    else if(y>a && a>b && a>c)
-        cout << "TAK";
-    else if(z>b && b>a && b>c)
-        cout << "TAK";
-    else if(a+b == b+c && a>0 && b>0 && c>0)
-        cout << "TAK";
-    else if(a==0 || b==0 || c==0)
-        cout << "NIE";
     else
         cout << "NIE";
+    
+    return 0;
 }
