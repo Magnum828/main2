d1afbceeb5c03adb123610c5faac3fec13b502c1
k2l8m11n2 <maurycy@piecha.xyz>
2018-10-15 08:42:45 +0200

    Pomiary

---
+++ pom.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int c = 0;
+	int lx = 0;
+	int x = 0;
+	while (true) {
+		cin >> x;
+		if (x == -1) {
+			break;
+		}
+		if (x != lx) {
+			c++;
+		}
+		lx = x;
+	}
+	cout << c << "\n";
+}
