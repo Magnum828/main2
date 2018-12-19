5970ac9e5bebef4e079b9ff475b8c46979a167fa
Marble <maurycy@piecha.xyz>
2018-12-20 00:43:12 +0100

    Formatting

--- bisekcja.hs
+++ bisekcja.hs
@@ -1,8 +1,13 @@
 avg a b = (a+b)/2
-bisect f (u, l) = if (f u) * (f s) < 0 then (u, s) else (s, l) where s = avg u l
+
+bisect f (u, l) | (f u) * (f s) < 0 = (u, s)
+                | otherwise = (s, l)
+                  where s = avg u l
 
 epsilon = 1e-10
 start = (0, 2)
 function x = x^2 - 2
 
-main = putStrLn $ show $ (uncurry avg) $ until (\(x, y) -> abs (x - y) < epsilon) (bisect function) start
+main = putStrLn $ show $ (uncurry avg) $
+  until (\(x, y) -> abs (x - y) < epsilon)
+    (bisect function) start
